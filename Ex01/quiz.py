07565879be59a8b9970affdf6be4e07b3cddc464
Hajime <c0a21028bd@edu.teu.ac.jp>
2022-06-14 16:09:53 +0900

    まだ終わってません

---
+++ Ex01/quiz.py
@@ -0,0 +1,43 @@
+import random
+import datetime
+import re
+
+
+def shutudai():
+    qs = [["サザエの旦那の名前は？",["マスオ","ますお"]],
+    ["カツオの妹の名前は？",["ワカメ","わかめ"]],
+    ["タラオはカツオから見てどんな関係？",["甥","おい","甥っ子","おいっこ"]]]
+
+    x1 = random.randint(0,len(qs)-1)
+    qX = qs[x1][0]
+    print("問題：")   
+    print(qX)
+    return qs[x1]
+
+def kaito():
+    ans = input("回答を入力/")
+    for i in q_and_a[1]:
+        if i == ans:
+            print("正解！")
+            return
+        
+    print("出直してこい")
+
+
+q_and_a = shutudai()
+kaito()
+
+#まだ終わってません
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
--- test.py
+++ test.py
@@ -1,1 +1,17 @@
 print("hello world")
+
+single = [2,4,6,8,10]
+twice = list()
+
+for i in single:
+    twice.append(i*2)
+
+print(twice)
+
+scores = list()
+while True:
+    y = int(input("リストに入れる値"))
+    if y < 0:
+        break
+    else:
+        scores.append(y)

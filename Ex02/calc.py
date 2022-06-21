eeca060ae96a13e4690da047161599a29ccf644c
Hajime <c0a21028bd@edu.teu.ac.jp>
2022-06-21 15:34:46 +0900

    練習問題4

--- Ex02/calc.py
+++ Ex02/calc.py
@@ -1,8 +1,10 @@
 from re import L
+from turtle import right
 from pip import main
 from cProfile import label
 import tkinter as tk
 import tkinter.messagebox as tkm
+
 
 def num_button_click(event):
     btn = event.widget
@@ -11,23 +13,30 @@ def num_button_click(event):
 
 
 if __name__ == "__main__":
-
-
     root = tk.Tk()
-
     root.title("calc")
     #root.geometry("300x500")
 
-    r, c = 0, 0 #r:行番号 c:列番号
-    for num in range(9,-1,-1):
+    entry = tk.Entry(width = 10,
+                    justify = "right",
+                    font = "OPTITimes-Roman, 40"
+                    )
+    #entry.insert(tk.END,)
+    entry.grid(columnspan=10)
+
+    r, c = 1, 0 #r:行番号 c:列番号
+    for num in range(9, -1, -1):
         btn = tk.Button(root,
                         text = f"{num}",
                         width = 4,
                         height = 2,
-                        font = ("SourceHanSerif-Bold,30")
+                        font = ("OPTITimes-Roman,30")
                         )
+
         btn.bind("<1>",num_button_click)
-        btn.grid(row = r, column = c)
+
+        btn.grid(row = r,
+                column = c)
         c += 1
         if (num-1)%3 == 0:
             r += 1
